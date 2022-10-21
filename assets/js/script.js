392c07271a423abd3b6cb69771fdc5e3d01c1c75
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2022-10-20 23:07:35 -0300

    função de vira a carta emplementada, falta resolver as questão de comparar as cartas!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,58 @@
-const array = [];
-let conteiner = document.querySelector('.conteiner')
 
-let numberCartas = 0;
-while(numberCartas < 4 || numberCartas > 21 || numberCartas % 2 !== 0){
+function iniciar(){
+    const imgCards = [
+        'arara.jpg',
+        'arara.jpg',
+        'tucano.jpg',
+        'tucano.jpg',
+        'ovelha.jpg',
+        'ovelha.jpg',
+        'coala.jpg',
+        'coala.jpg',
+        'flamigo.png',
+        'flamigo.png',
+        'onca.png',
+        'onca.png',
+        'pombo.jpg',
+        'pombo.jpg'
+    ]
+    const cartas = [];
+    const conteiner = document.querySelector('.conteiner')
+
+    let numberCartas = 0;
+    while(numberCartas < 4 || numberCartas > 14 || numberCartas % 2 !== 0){
         numberCartas = Number(prompt("DIGITE A QUANTIDADE DE CARTAS: "));
-}
-
-let cont = 0;
-while(numberCartas > cont){
-        array.push(`<li class="card">
+    } 
+    
+    for(let i = 0;numberCartas > i; i++){
+        cartas.push(`<li onclick="clicar(this)" class="card">
         <div class="frente face">
             <img src="assets/img/back.png">
         </div>
         <div class="verso face">
-            <img src="assets/img/bobrossparrot.gif">
+            <img src="assets/img/${imgCards[i]}">
         </div>
-    </li>`)
-        cont++
+    </li>`) 
+    }
+
+    cartas.sort(embaralhador);
+
+    for(let i = 0;numberCartas > i; i++){
+        conteiner.innerHTML += cartas[i];
+    }
 }
 
-cont = 0;
-while(numberCartas > cont){
-        conteiner.innerHTML += array[cont];
-        cont++
+function embaralhador(){
+    return Math.random() -0.5;
 }
 
-function clicador(){
-       alert('clicado')
+
+let clickJogadas = 0;
+function clicar(virar){
+    virar.classList.toggle('click');
 }
+
+
+
+iniciar();
+embaralhador()

f6b8f92cd9d80e5814b1d3d2c04bb70b0ef41515
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2022-10-23 23:44:32 -0300

    redirecionamento da pagina feito

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ function iniciarGamer(){
     const nome = document.querySelector('.nome').value;
     const numero = document.querySelector('.numberDeCartas').value;
 
-    localStorage.nomePLayer = JSON.stringify(nome)
-    localStorage.NumeroDeCartas = JSON.stringify(numero)
+    localStorage.nomePLayer = JSON.stringify(nome);
+    localStorage.NumeroDeCartas = JSON.stringify(numero);
+    window.open('./gamer.html');
 }
